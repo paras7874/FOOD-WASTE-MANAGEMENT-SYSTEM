0aa51fb1c757c22aa30459f22b60ca2c5551ce9e
Paras Marwadi <parasmarwadi024@gmail.com>
2026-07-23 14:50:45 +0530

    remove file

--- food_donations_system/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-<div class="container">
-      <p class="mb-0">&copy; 2025 Food Waste Management System | Fighting Hunger Together</p>
-</div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
